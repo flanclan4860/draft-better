7ac3d89b70a87384c0630572374f5dc6638bd598
kevinkrenz <kevin.krenz@gmail.com>
2014-01-27 16:32:35 -0600

    Starting to setup the UI.

--- server.R
+++ server.R
@@ -1,7 +1,30 @@
 library(shiny)
 
+load('projections.RData')
+
 shinyServer(function(input, output) {
   
   
+  # Listen for additions to teams
+  observe({
+    if (input$add < 1){
+      return()
+    }
+    
+    # The add button was clicked, so update the player's team and rosters
+    isolate({
+      projections.df[projections.df$Player == input$playerChosen, 
+                     'fantasyTeam'] = input$teamChosen
+      save(projections.df, file='projections.RData')
+    })
+  })
+  
+  output$team1 <- renderTable({
+    subset(projections.df, fantasyTeam == 'Team 1')[,1:7]
+  })
+  
+  output$team2 <- renderTable({
+    subset(projections.df, fantasyTeam == 'Team 2')[,1:7]
+  })
   
 })
---
+++ ui_rosters.R
@@ -0,0 +1,9 @@
+tabPanel(
+  'Rosters',
+  
+  h4('Team 1'),
+  tableOutput('team1'),
+  h4('Team 2'),
+  tableOutput('team2')
+  
+)
