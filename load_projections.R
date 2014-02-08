60e1e971e7ee4d50be0654b3778096f72fe463a9
Kevin Krenz <kevin.krenz@gmail.com>
2014-02-08 12:30:28 -0600

    Added function for loading data from FantasyPros. Renamed load_data to load_data_ESPN.

--- load_projections.R
+++ load_projections.R
@@ -7,7 +7,7 @@ library(stringr)
 source('settings.R', local=TRUE)
 
 # Function to load ESPN's projected statistics
-load_data <- function() {
+load_data_ESPN <- function() {
   # Load ESPN's hitting projections.
   # This function is not particularly efficient, but it works.
   
@@ -56,6 +56,83 @@ load_data <- function() {
   projections.df$Pos3 <- as.factor(projections.df$Pos3)
   projections.df$Pos4 <- as.factor(projections.df$Pos4)
   projections.df$Pos5 <- as.factor(projections.df$Pos5)
+  
+  # Add columns for drafted and fantasyTeam
+  n <- dim(projections.df)[1]
+  projections.df <- cbind(projections.df,
+                          drafted=rep(NA, n),
+                          fantasyTeam=rep('', n))
+  
+  # Set fantasy teams as factors
+  # NOT WORKING. I need to learn more about factors...
+  # projections.df$fantasyTeam <- as.factor(projections.df$fantasyTeam,
+  #                                        levels=teams)
+  
+  # Set draft pick as integer
+  projections.df$drafted <- as.integer(projections.df$drafted)
+  
+  projections.df
+}
+
+load_data_fantasypros <- function() {
+  # Load FantasyPro's hitting projections.
+  # This function is not particularly efficient, but it works.
+  
+  # Load projections
+  url <- 'http://www.fantasypros.com/mlb/projections/hitters.php?page=ALL'
+  classes <- c('character', rep('numeric', 15))
+  projections.df <- readHTMLTable(url, which=2, colClasses=classes)
+  
+  
+  # Split the player column into useful pieces.
+  # This part in particular could be way better; I'm learning.
+  
+  # Rename first column
+  colnames(projections.df)[1] <- 'Player'
+  
+  # TODO: strip end parentheses and white space
+  
+  # Split player column into name and team/pos
+  projections.df <- with(projections.df,
+                         cbind(colsplit(projections.df$Player,
+                                        pattern='\\(',
+                                        names=c('Player', 'Team.Pos')),
+                               projections.df[,2:16]))
+  
+  # Split team/pos column into team and pos
+  projections.df <- with(projections.df,
+                         cbind(Player,
+                               colsplit(projections.df$Team.Pos,
+                                        pattern='\\,',
+                                        names=c('Team', 'Pos')),
+                               projections.df[,3:17]))
+  
+  # Split positions
+  projections.df <- with(projections.df,
+                         cbind(Player, Team,
+                               colsplit(projections.df$Pos,
+                                        pattern='\\/',
+                                        names=c('Pos1', 'Pos2')),
+                               projections.df[,4:18]))
+  projections.df <- with(projections.df,
+                         cbind(Player, Team, Pos1, 
+                               colsplit(projections.df$Pos2,
+                                        pattern='\\/',
+                                        names=c('Pos2', 'Pos3')),
+                               projections.df[,5:19]))
+  projections.df <- with(projections.df,
+                         cbind(Player, Team, Pos1, Pos2,
+                               colsplit(projections.df$Pos3,
+                                        pattern='\\/',
+                                        names=c('Pos3', 'Pos4')),
+                               projections.df[,6:20]))
+  
+  # Convert positions and teams to factors
+  projections.df$Team <- as.factor(projections.df$Team)
+  projections.df$Pos <- as.factor(projections.df$Pos1)
+  projections.df$Pos2 <- as.factor(projections.df$Pos2)
+  projections.df$Pos3 <- as.factor(projections.df$Pos3)
+  projections.df$Pos4 <- as.factor(projections.df$Pos4)
   
   # Add columns for drafted and fantasyTeam
   n <- dim(projections.df)[1]
